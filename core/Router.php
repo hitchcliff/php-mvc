b57795c6bf94ce3586c2adb50d53ba76e6c9af66
kevin700brands <kevin@700brands.com>
2022-09-11 19:14:59 +0800

    routing

--- core/Router.php
+++ core/Router.php
@@ -3,7 +3,27 @@
 namespace app\core;
 
 class Router {
-  public function __construct() {
-    echo 'hello from router';
+  public Request $request;
+  protected array $routes =[];
+
+  public function __construct(\app\core\Request $request) {
+    $this->request = $request;
+  }
+
+  public function get($path, $callback)  {
+   $this->routes['get'][$path]  = $callback;
+  }
+
+  public function resolve() {
+    $path = $this->request->getPath();
+    $method = $this->request->getMethod();
+    $callback = $this->routes[$method][$path] ?? false;
+
+    if($callback === false) {
+      echo "Not found";
+      exit;
+    }
+
+    echo call_user_func($callback);
   }
 }
